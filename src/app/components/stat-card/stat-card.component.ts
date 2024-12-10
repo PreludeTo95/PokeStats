fff5ef053cd28a7e0e98e02c283acc4455c24344
Aaron Tiger <tigeraaronm@gmail.com>
2024-12-10 00:28:02 -0500

    stat section finalized

--- src/app/components/stat-card/stat-card.component.ts
+++ src/app/components/stat-card/stat-card.component.ts
@@ -14,6 +14,24 @@ export class StatCardComponent {
   currentPokemon!: Pokemon;
   showMoves: boolean = false;
   maxChartValue: number = 285;
+  statNames: string[] = ['HP','Attack','Defense','Sp. Atk','Sp. Def','Speed'];
+
+  statColors = new Map<string, string>([
+    ["red", "#F34444"],
+    ["orange", "#FF7F0F"],
+    ["yellow", "#FFDD57"],
+    ["light-green", "#A0E515"],
+    ["dark-green", "#23CD5E"],
+    ["blue", "#00C2B8"]
+  ]);
+
+  statValues = new Map<string, number>([
+    ["red", 30],
+    ["orange", 60],
+    ["yellow", 90],
+    ["light-green", 120],
+    ["dark-green", 150],
+  ]);
 
   constructor(
     private pokemonService: PokemonService,
@@ -34,4 +52,26 @@ export class StatCardComponent {
   calculateStatBarLength(statValue: number) {
     return ((100 * statValue / this.maxChartValue).toString() + '%');
   }
+
+  calculateStatBarColor(statValue: number) {
+    if (statValue < this.statValues.get("red")!) {
+      return this.statColors.get("red");
+    } 
+    else if (statValue < this.statValues.get("orange")!) {
+      return this.statColors.get("orange");
+    } 
+    else if (statValue < this.statValues.get("yellow")!) {
+      return this.statColors.get("yellow");
+    } 
+    else if (statValue < this.statValues.get("light-green")!) {
+      return this.statColors.get("light-green");
+    } 
+    else if (statValue < this.statValues.get("dark-green")!) {
+      return this.statColors.get("dark-green");
+    } 
+    else {
+      return this.statColors.get("blue");
+    }
+  }
+
 }
