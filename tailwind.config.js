9958e2af0478d0b9bbcfaf8143e9679477799052
Aaron Tiger <tigeraaronm@gmail.com>
2024-12-15 20:50:30 -0500

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/app/app.component.html",
+    "./src/app/components/pokemon-card/pokemon-card.component.html",
+    "./src/app/components/search-bar/search-bar.component.html",
+    "./src/app/components/stat-card/stat-card.component.html",
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [
+
+  ],
+}
+
